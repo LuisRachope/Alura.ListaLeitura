12bb2fcfcd4ad8061307b9c25f7033a0d7920690
Luis Rachope <luis.chiasso@hotmail.com>
2023-03-19 13:33:14 -0300

    Limpeza da classe Startup e direcionamento dos arquivos

---
+++ Alura.ListaLeitura.App/View/HtmlUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Alura.ListaLeitura.App.View
+{
+    public class HtmlUtils
+    {
+        public static string CarregaArquivoHTML(string nomeArquivo)
+        {
+            var nomeCompletoArquivo = $"View/{nomeArquivo}.html";
+            using (var arquivo = File.OpenText(nomeCompletoArquivo))
+            {
+                return arquivo.ReadToEnd();
+            }
+        }
+    }
+}
